f4fb71d873f33becc419f657602ea692e2d8fc85
jinovative <jinyung7428@gmail.com>
2025-04-08 21:17:35 -0400

    part2 and 3

--- client/rfs.c
+++ client/rfs.c
@@ -10,6 +10,8 @@
 #define SERVER_PORT 2024
 #define BUFFER_SIZE 1024
 
+void handle_rm(int client_sock, const char *path);
+
 void write_command(const char *local_path, const char *remote_path) {
     // 1. open file
     FILE *fp = fopen(local_path, "rb");
@@ -46,11 +48,79 @@ void write_command(const char *local_path, const char *remote_path) {
     printf("File sent successfully.\n");
 }
 
+void get_command(const char *remote_path, const char *local_path) {
+    // 1. connect to server
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+    // 2. request file
+    dprintf(sock, "GET\n%s\n", remote_path);
+
+    // 3. receive file size
+    char header[100];
+    read(sock, header, sizeof(header));
+    long file_size = atol(header);  // 크기 파싱
+
+    // 4. save file
+    FILE *fp = fopen(local_path, "wb");
+    if (!fp) {
+        perror("Cannot open file to write");
+        close(sock);
+        return;
+    }
+
+    char buffer[BUFFER_SIZE];
+    long received = 0;
+    while (received < file_size) {
+        ssize_t n = read(sock, buffer, BUFFER_SIZE);
+        fwrite(buffer, 1, n, fp);
+        received += n;
+    }
+
+    fclose(fp);
+    close(sock);
+    printf("File received and saved to %s\n", local_path);
+}
+
+void rm_command(const char *remote_path) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+    dprintf(sock, "RM\n%s\n", remote_path);
+
+    char buffer[BUFFER_SIZE];
+    read(sock, buffer, sizeof(buffer));
+    printf("Server response: %s\n", buffer);
+
+    close(sock);
+}
+
 int main(int argc, char *argv[]) {
-    if (argc != 4 || strcmp(argv[1], "WRITE") != 0) {
-        fprintf(stderr, "Usage: %s WRITE local_file remote_file\n", argv[0]);
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s COMMAND args...\n", argv[0]);
         return 1;
     }
-    write_command(argv[2], argv[3]);
+
+    if (strcmp(argv[1], "WRITE") == 0 && argc == 4) {
+        write_command(argv[2], argv[3]);
+    } else if (strcmp(argv[1], "GET") == 0 && argc == 4) {
+        get_command(argv[2], argv[3]);
+    } 
+    else if (strcmp(argv[1], "RM") == 0 && argc == 3) {
+        rm_command(argv[2]);
+    }
+    else {
+        fprintf(stderr, "Invalid usage.\n");
+        return 1;
+    }
+
     return 0;
 }
--- server/rfserver.c
+++ server/rfserver.c
@@ -8,40 +8,80 @@
 #define PORT 2024
 #define BUFFER_SIZE 1024
 
+void handle_get(int client_sock, const char *path);
+void handle_rm(int client_sock, const char *path);
+
 void handle_client(int client_sock) {
     char buffer[BUFFER_SIZE];
-
-    // 1. get command, remote path, file size
     read(client_sock, buffer, sizeof(buffer));
+
     char *cmd = strtok(buffer, "\n");
-    if (strcmp(cmd, "WRITE") != 0) {
-        fprintf(stderr, "Unsupported command: %s\n", cmd);
-        close(client_sock);
-        return;
-    }
-    char *remote_path = strtok(NULL, "\n");
-    char *file_size_str = strtok(NULL, "\n");
-    long file_size = atol(file_size_str);
+    char *arg1 = strtok(NULL, "\n");
+    char *arg2 = strtok(NULL, "\n");  // only for WRITE
 
-    // 2. write file to disk
-    FILE *fp = fopen(remote_path, "wb");
+    if (strcmp(cmd, "WRITE") == 0) {
+        // handle write command
+        long file_size = atol(arg2);
+        FILE *fp = fopen(arg1, "wb");
+        long received = 0;
+        while (received < file_size) {
+            ssize_t n = read(client_sock, buffer, BUFFER_SIZE);
+            fwrite(buffer, 1, n, fp);
+            received += n;
+        }
+        fclose(fp);
+    }
+    else if (strcmp(cmd, "GET") == 0) {
+        handle_get(client_sock, arg1);
+    }
+
+    else if (strcmp(cmd, "RM") == 0) {
+        handle_rm(client_sock, arg1);
+    }
+
+    close(client_sock);
+}
+
+void handle_get(int client_sock, const char *path) {
+    FILE *fp = fopen(path, "rb");
     if (!fp) {
-        perror("Cannot create file on server");
-        close(client_sock);
+        perror("File not found on server");
+        dprintf(client_sock, "0\n"); // not found
         return;
     }
 
-    long received = 0;
-    while (received < file_size) {
-        ssize_t n = read(client_sock, buffer, BUFFER_SIZE);
-        fwrite(buffer, 1, n, fp);
-        received += n;
+    // calculate file size
+    fseek(fp, 0, SEEK_END);
+    long fsize = ftell(fp);
+    rewind(fp);
+    dprintf(client_sock, "%ld\n", fsize);
+
+    // send file data
+    char buffer[BUFFER_SIZE];
+    long sent = 0;
+    while (sent < fsize) {
+        size_t n = fread(buffer, 1, BUFFER_SIZE, fp);
+        send(client_sock, buffer, n, 0);
+        sent += n;
     }
 
     fclose(fp);
-    printf("Received and saved file to %s\n", remote_path);
-    close(client_sock);
 }
+
+#include <errno.h>
+
+void handle_rm(int client_sock, const char *path) {
+    int result = remove(path);
+    if (result == 0) {
+        dprintf(client_sock, "OK\n");
+        printf("Deleted file: %s\n", path);
+    } else {
+        dprintf(client_sock, "FAIL\n");
+        perror("Delete failed");
+    }
+}
+
+
 
 int main() {
     int server_sock = socket(AF_INET, SOCK_STREAM, 0);
